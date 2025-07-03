fd8960cb3306f1b2c92e0c8d1e76e487d8ce1142
francescoboschelle <francibossch@gmail.com>
2025-07-03 12:17:05 +0200

    feat: add error handling middleware for improved API response management

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import e from "express";
 import postsRouter from "./routers/posts.js";
+import { routeNotFound, errorHandler } from "./middlewares/middleware.js";
 
 const app = e();
 const port = 3000;
@@ -11,6 +12,9 @@ app.use("/posts", postsRouter);
 app.get("/", (req, res) => {
   res.send("Express Blog API Crud");
 });
+
+app.use(errorHandler);
+app.use(routeNotFound);
 
 app.listen(port, () => {
   console.log(`Server in ascolto sulla porta ${port}`);
